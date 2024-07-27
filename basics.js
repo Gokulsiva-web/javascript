1407150af4aeecf02a225bd0bff4f917c2037d07
Gokulsiva-web <gokulsivamaharaja@gmail.com>
2024-07-27 19:22:52 +0530

    String

---
+++ basics.js
@@ -0,0 +1,21 @@
+// // const account_id =144533
+// // let accountEmail="gokulsiva@gmail.com"
+// // var account_password="1234"
+// // accountcity="jaipur"
+// // console.log(accountEmail)
+// // console.table([accountEmail,account_id,account_password,accountcity])
+// const name="hello"
+// const value="world"
+// // console.log(name)
+// console.log(`${name} ${ value.toUpperCase}`)
+const myname = new String("gokulsivga");
+// console.log(myname[3])
+// console.log(myname.__proto__);
+// console.log(myname.length)
+// console.log(myname.charAt(2))
+const newn = myname.substring(0, 9);
+console.log(newn);
+function add(a, b) {
+  return a - b;
+  
+}
